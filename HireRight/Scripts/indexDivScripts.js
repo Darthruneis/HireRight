b144ab4289bb9be999f3b8396a302890ba3f6de9
Chris <Chris@CHRIS-PC>
2017-01-14 16:49:54 -0500

    Fixed some broken javascript by reworking it to be simpler, slightly redid some styling.

--- HireRight/Scripts/indexDivScripts.js
+++ HireRight/Scripts/indexDivScripts.js
@@ -1,9 +1,8 @@
-﻿function displayInfoDiv(data) {
-    $("#infoDiv").html($(data).innerHTML);
-    $("#infoDiv").attr("hidden", false);
-    $("#introDiv").attr("hidden", true);
-}
-function displayIntroDiv() {
-    $("#infoDiv").attr("hidden", true);
-    $("#introDiv").attr("hidden", false);
+﻿$(document).ready(function () {
+    replaceInfoDivHtmlWith("#introDiv");
+});
+
+function replaceInfoDivHtmlWith(data) {
+    var innerhtml = $(data).html();
+    $("#infoDiv").html(innerhtml);
 }
