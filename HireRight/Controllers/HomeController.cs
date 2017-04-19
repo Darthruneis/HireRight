56fb8c51270a1c323e7f434a100bf32d9b4ff7bd
Chris <Chris@CHRIS-PC>
2017-04-19 19:56:44 -0400

    Implemented a tooltip for the collapsible sections which explains what the buttons do.

--- HireRight/Controllers/HomeController.cs
+++ HireRight/Controllers/HomeController.cs
@@ -21,13 +21,6 @@ namespace HireRight.Controllers
         public PartialViewResult OccupationalCategories() => PartialView("WhoWeServePartial");
 
         [HttpGet]
-        public ViewResult Test()
-        {
-            SectionWithTogglesAndTitleViewModel model = new SectionWithTogglesAndTitleViewModel(" show the order form again.", "hiddenContent", "hideButton", "showButton", "shownContent", "~/Views/Home/Index.cshtml", "Title");
-            return View("SectionWithPanelHeadAndToggles", model);
-        }
-
-        [HttpGet]
         public PartialViewResult WhyUseHireRight() => PartialView("WhyUseHireRightPartial");
     }
 }
