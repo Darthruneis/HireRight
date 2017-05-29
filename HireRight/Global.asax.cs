59f564d1c1482b5b2a1fcf4c321b3df4f7b6564b
Chris <Chris@CHRIS-PC>
2017-05-28 23:54:41 -0400

    Added exception logging in a very basic way, and turned on custom errors on the Live environment to assist with debugging.

--- HireRight/Global.asax.cs
+++ HireRight/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.IO;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,6 +10,19 @@ namespace HireRight
 {
     public class MvcApplication : HttpApplication
     {
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+
+            using (StreamWriter writer = new StreamWriter(File.Open("~/log.txt", FileMode.OpenOrCreate)))
+            {
+                writer.WriteLine("--- START");
+                writer.WriteLine("Exception logged at: " + DateTime.Now);
+                writer.Write(exception);
+                writer.WriteLine("--- END");
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
