7b459043ae8a0aaeb1d964e28bd4158db616a090
Chris <Chris@CHRIS-PC>
2018-03-04 22:38:23 -0500

    Reworded the form instructions and wired up the continue button to toggle irrelevant categories.

--- HireRight/Scripts/CustomSolutions/_customSolutions.js
+++ HireRight/Scripts/CustomSolutions/_customSolutions.js
@@ -1,5 +1,30 @@
 var CustomSolutions;
 (function (CustomSolutions) {
+    function bindEvents() {
+        $("#categoryContainerDiv").on("click", ".lessImportantButton", function (e) {
+            decreaseImportanceLevel($(this).closest(".categoryCardRow"));
+        });
+        $("#categoryContainerDiv").on("click", ".moreImportantButton", function (e) {
+            increaseImportanceLevel($(this).closest(".categoryCardRow"));
+        });
+        $("#ContinueButton").on("click", function () { toggleIrrelevantCards(); });
+    }
+    CustomSolutions.bindEvents = bindEvents;
+    function toggleIrrelevantCards() {
+        $(".importanceHeaders").find(":not(:first-child)").toggleClass("col-xs-4 col-xs-6");
+        $(".importanceHeaders").find(":first-child").toggle();
+        $(".categoryCardRow input[type='hidden']").each(function (index, elem) {
+            var $row = $(elem).closest(".categoryCardRow");
+            var $newValue = $(elem).val();
+            if ($newValue === "Irrelevant") {
+                $row.toggle();
+            }
+            else {
+                $row.find(".col-xs-4, .col-xs-6").toggleClass("col-xs-4 col-xs-6").first().toggle();
+            }
+            toggleButtonsBasedOnImportance($row, $newValue);
+        });
+    }
     function getImportanceLevel($categoryRow) {
         return $categoryRow.find("input[type='hidden']").val();
     }
@@ -22,13 +47,7 @@ var CustomSolutions;
             return "Irrelevant";
         return "LowImportance";
     }
-    function updateImportanceLevel($categoryRow, increase) {
-        var current = getNumericImportanceLevel(getImportanceLevel($categoryRow));
-        if (increase)
-            current = current + 1;
-        else
-            current = current - 1;
-        var newValue = getStringImportanceLevel(current);
+    function toggleButtonsBasedOnImportance($categoryRow, newValue) {
         if (newValue === "HighImportance") {
             $categoryRow.find(".moreImportantButton").hide();
         }
@@ -39,6 +58,15 @@ var CustomSolutions;
             $categoryRow.find(".moreImportantButton").show();
             $categoryRow.find(".lessImportantButton").show();
         }
+    }
+    function updateImportanceLevel($categoryRow, increase) {
+        var current = getNumericImportanceLevel(getImportanceLevel($categoryRow));
+        if (increase)
+            current = current + 1;
+        else
+            current = current - 1;
+        var newValue = getStringImportanceLevel(current);
+        toggleButtonsBasedOnImportance($categoryRow, newValue);
         $categoryRow.find("input[type='hidden']").val(newValue);
         var detachedHtml = $categoryRow.find(".categoryCard").detach();
         detachedHtml.appendTo($categoryRow.find(".col-xs-4")[getNumericImportanceLevel(newValue)]);
@@ -49,14 +77,5 @@ var CustomSolutions;
     function decreaseImportanceLevel($categoryRow) {
         updateImportanceLevel($categoryRow, false);
     }
-    function bindEvents() {
-        $("#categoryContainerDiv").on("click", ".lessImportantButton", function (e) {
-            decreaseImportanceLevel($(this).closest(".categoryCardRow"));
-        });
-        $("#categoryContainerDiv").on("click", ".moreImportantButton", function (e) {
-            increaseImportanceLevel($(this).closest(".categoryCardRow"));
-        });
-    }
-    CustomSolutions.bindEvents = bindEvents;
 })(CustomSolutions || (CustomSolutions = {}));
 //# sourceMappingURL=_customSolutions.js.map
--- HireRight/Scripts/CustomSolutions/_customSolutions.ts
+++ HireRight/Scripts/CustomSolutions/_customSolutions.ts
@@ -1,7 +1,37 @@
 ﻿namespace CustomSolutions {
+    export function bindEvents() {
+        $("#categoryContainerDiv").on("click",
+            ".lessImportantButton",
+            function (e) {
+                decreaseImportanceLevel($(this).closest(".categoryCardRow"));
+            });
+        $("#categoryContainerDiv").on("click",
+            ".moreImportantButton",
+            function (e) {
+                increaseImportanceLevel($(this).closest(".categoryCardRow"));
+            });
+        $("#ContinueButton").on("click", () => { toggleIrrelevantCards(); });
+    }
+
+    function toggleIrrelevantCards(): void {
+        $(".importanceHeaders").find(":not(:first-child)").toggleClass("col-xs-4 col-xs-6");
+        $(".importanceHeaders").find(":first-child").toggle();
+        $(".categoryCardRow input[type='hidden']").each((index, elem) => {
+            var $row = $(elem).closest(".categoryCardRow");
+            var $newValue = $(elem).val();
+            if ($newValue === "Irrelevant") {
+                $row.toggle();
+            } else {
+                $row.find(".col-xs-4, .col-xs-6").toggleClass("col-xs-4 col-xs-6").first().toggle();
+            }
+            toggleButtonsBasedOnImportance($row, $newValue);
+        });
+    }
+
     function getImportanceLevel($categoryRow: JQuery): string {
         return $categoryRow.find("input[type='hidden']").val();
     }
+
     function getNumericImportanceLevel(stringValue: string): number {
         switch (stringValue) {
             case "Irrelevant":
@@ -15,6 +45,7 @@
                 return 1;
         }
     }
+
     function getStringImportanceLevel(intValue: number): string {
         if (intValue >= 2)
             return "HighImportance";
@@ -22,14 +53,8 @@
             return "Irrelevant";
         return "LowImportance";
     }
-    function updateImportanceLevel($categoryRow: JQuery, increase: boolean): void {
-        var current = getNumericImportanceLevel(getImportanceLevel($categoryRow));
-        if (increase)
-            current = current + 1;
-        else
-            current = current - 1;
 
-        var newValue = getStringImportanceLevel(current);
+    function toggleButtonsBasedOnImportance($categoryRow: JQuery, newValue: string): void {
         if (newValue === "HighImportance") {
             $categoryRow.find(".moreImportantButton").hide();
         }
@@ -39,30 +64,29 @@
             $categoryRow.find(".moreImportantButton").show();
             $categoryRow.find(".lessImportantButton").show();
         }
+    }
+
+    function updateImportanceLevel($categoryRow: JQuery, increase: boolean): void {
+        var current = getNumericImportanceLevel(getImportanceLevel($categoryRow));
+        if (increase)
+            current = current + 1;
+        else
+            current = current - 1;
+
+        var newValue = getStringImportanceLevel(current);
+        toggleButtonsBasedOnImportance($categoryRow, newValue);
 
         $categoryRow.find("input[type='hidden']").val(newValue);
 
         var detachedHtml = $categoryRow.find(".categoryCard").detach();
         detachedHtml.appendTo($categoryRow.find(".col-xs-4")[getNumericImportanceLevel(newValue)]);
     }
+
     function increaseImportanceLevel($categoryRow): void {
         updateImportanceLevel($categoryRow, true);
     }
+
     function decreaseImportanceLevel($categoryRow): void {
         updateImportanceLevel($categoryRow, false);
-    }
-
-    export function bindEvents() {
-        $("#categoryContainerDiv").on("click",
-            ".lessImportantButton",
-            function (e) {
-                decreaseImportanceLevel($(this).closest(".categoryCardRow"));
-            });
-        $("#categoryContainerDiv").on("click",
-            ".moreImportantButton",
-            function (e) {
-                increaseImportanceLevel($(this).closest(".categoryCardRow"));
-            });
-
     }
 }
